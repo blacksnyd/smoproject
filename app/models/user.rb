2dcf618e7062ee68399aa6a1d162e43f8d8d48d8
Kenzo douchet <kenzodouchet33@gmail.com>
2023-11-09 03:39:03 +0100

    add :trackable to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :trackable
+
   geocoded_by :current_sign_in_ip
   after_validation :geocode, if: :current_sign_in_ip_changed?
 
